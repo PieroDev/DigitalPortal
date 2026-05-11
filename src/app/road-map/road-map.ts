32325af991d4c424900db0c826a6532e22a634ac
Piero Zúñiga <piero_xz@hotmail.com>
2026-05-11 12:04:17 -0400

    code upd

--- src/app/road-map/road-map.ts
+++ src/app/road-map/road-map.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, AfterViewInit, ViewChild, ChangeDetectorRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-road-map',
-  imports: [],
-  templateUrl: './road-map.html',
-  styleUrl: './road-map.css',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './road-map.html'
 })
-export class RoadMap {
+export class RoadMap implements AfterViewInit {
+  @ViewChild('roadmapContainer') roadmapContainer!: ElementRef;
+  isVisible = false;
 
+  constructor(private cdr: ChangeDetectorRef) {}
+
+  ngAfterViewInit() {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting) {
+          this.isVisible = true;
+          this.cdr.detectChanges(); 
+          observer.unobserve(this.roadmapContainer.nativeElement);
+        }
+      },
+      { threshold: 0.1 } 
+    );
+
+    if (this.roadmapContainer) observer.observe(this.roadmapContainer.nativeElement);
+  }
 }
